b349e9997fe73d919dd48a6cb01b2ef33f252834
jonesarn <ajones10196@icloud.com>
2022-05-04 06:44:51 -0400

    Establishing the user table

--- ElevenNote.Data/ApplicationDbContext.cs
+++ ElevenNote.Data/ApplicationDbContext.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using ElevenNote.Data.Entities;
 
 namespace ElevenNote.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) 
+            : base(options)
         {
-
+        }
+            public DbSet<UserEntity> Users {get; set;}
         }
     }
-}
